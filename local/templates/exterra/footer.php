ebd71f07209d0a31c9ab0e8246a43625d362179b
Random4405 <random4405@gmail.com>
2017-03-04 20:10:37 +0300

    Added news infoblock

--- local/templates/exterra/footer.php
+++ local/templates/exterra/footer.php
@@ -2,6 +2,97 @@
 IncludeTemplateLangFile(__FILE__);
 ?>
 
+<div class="sell">
+  <div class="container">
+    <div class="row">
+      <div class="left-wrapper">
+        <h1>Продажа строительных материалов</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur aliquid quia, et harum maiores voluptates dignissimos vero magni tempore. Eaque totam, et incidunt sit, excepturi aperiam ipsam repellat ad dolores nulla quae placeat iusto consectetur magni repellendus, eos id exercitationem ipsum tempora consequatur quas? Aspernatur harum maxime distinctio recusandae quasi?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur aliquid quia, et harum maiores voluptates dignissimos vero magni tempore. Eaque totam, et incidunt sit, excepturi aperiam ipsam repellat ad dolores nulla quae placeat iusto consectetur magni repellendus, eos id exercitationem ipsum tempora consequatur quas? Aspernatur harum maxime distinctio recusandae quasi?</p>
+      </div>
+      <div class="right-wrapper">
+        <?$APPLICATION->IncludeComponent(
+          "bitrix:news",
+          ".default",
+          Array(
+            "ADD_ELEMENT_CHAIN" => "N",
+            "ADD_SECTIONS_CHAIN" => "Y",
+            "AJAX_MODE" => "N",
+            "AJAX_OPTION_ADDITIONAL" => "",
+            "AJAX_OPTION_HISTORY" => "Y",
+            "AJAX_OPTION_JUMP" => "Y",
+            "AJAX_OPTION_SHADOW" => "N",
+            "AJAX_OPTION_STYLE" => "Y",
+            "BROWSER_TITLE" => "-",
+            "CACHE_FILTER" => "N",
+            "CACHE_GROUPS" => "Y",
+            "CACHE_TIME" => "36000000",
+            "CACHE_TYPE" => "A",
+            "CHECK_DATES" => "Y",
+            "COMPONENT_TEMPLATE" => ".default",
+            "DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",
+            "DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
+            "DETAIL_DISPLAY_TOP_PAGER" => "N",
+            "DETAIL_FIELD_CODE" => array(0=>"",1=>"",),
+            "DETAIL_PAGER_SHOW_ALL" => "N",
+            "DETAIL_PAGER_TEMPLATE" => "arrows",
+            "DETAIL_PAGER_TITLE" => "Страница",
+            "DETAIL_PROPERTY_CODE" => array(0=>"",1=>"",),
+            "DETAIL_SET_CANONICAL_URL" => "N",
+            "DISPLAY_BOTTOM_PAGER" => "Y",
+            "DISPLAY_DATE" => "Y",
+            "DISPLAY_NAME" => "Y",
+            "DISPLAY_PANEL" => "N",
+            "DISPLAY_PICTURE" => "Y",
+            "DISPLAY_PREVIEW_TEXT" => "Y",
+            "DISPLAY_TOP_PAGER" => "N",
+            "HIDE_LINK_WHEN_NO_DETAIL" => "Y",
+            "IBLOCK_ID" => "5",
+            "IBLOCK_TYPE" => "news",
+            "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+            "LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
+            "LIST_FIELD_CODE" => array(0=>"",1=>"",),
+            "LIST_PROPERTY_CODE" => array(0=>"",1=>"",),
+            "MESSAGE_404" => "",
+            "META_DESCRIPTION" => "-",
+            "META_KEYWORDS" => "-",
+            "NEWS_COUNT" => "5",
+            "NUM_DAYS" => "30",
+            "NUM_NEWS" => "20",
+            "PAGER_BASE_LINK_ENABLE" => "N",
+            "PAGER_DESC_NUMBERING" => "N",
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000000",
+            "PAGER_SHOW_ALL" => "N",
+            "PAGER_SHOW_ALWAYS" => "N",
+            "PAGER_TEMPLATE" => "",
+            "PAGER_TITLE" => "Новости",
+            "PREVIEW_TRUNCATE_LEN" => "",
+            "SEF_FOLDER" => "/news/",
+            "SEF_MODE" => "Y",
+            "SEF_URL_TEMPLATES" => array("news"=>"","section"=>"","detail"=>"#ELEMENT_ID#/","rss"=>"rss/","rss_section"=>"#SECTION_ID#/rss/",),
+            "SET_LAST_MODIFIED" => "N",
+            "SET_STATUS_404" => "Y",
+            "SET_TITLE" => "Y",
+            "SHOW_404" => "N",
+            "SORT_BY1" => "ACTIVE_FROM",
+            "SORT_BY2" => "SORT",
+            "SORT_ORDER1" => "DESC",
+            "SORT_ORDER2" => "ASC",
+            "USE_CATEGORIES" => "N",
+            "USE_FILTER" => "N",
+            "USE_PERMISSIONS" => "N",
+            "USE_RATING" => "N",
+            "USE_RSS" => "Y",
+            "USE_SEARCH" => "N",
+            "USE_SHARE" => "N",
+            "YANDEX" => "N"
+          )
+        );?>
+      </div>
+    </div>
+  </div>
+</div>
+
 <div class="form-wrapper">
   <?$APPLICATION->IncludeComponent("custom:main.feedback", "feedback_main", Array(
     "EMAIL_TO" => "dvsiteminsk@gmail.com",  // E-mail, на который будет отправлено письмо
