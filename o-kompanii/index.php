ce680baccffd1b460926831087ac5f717c4ac485
Random4405 <random4405@gmail.com>
2017-03-10 20:22:05 +0300

    Index page restyle

--- o-kompanii/index.php
+++ o-kompanii/index.php
@@ -3,6 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("О компании");
 ?>
 
-Text here....
+Hello
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
