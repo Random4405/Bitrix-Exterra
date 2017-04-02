3cbc0422886d8f740dd1c12388aabf3ce6e1f595
Random4405 <random4405@gmail.com>
2017-04-02 19:56:36 +0300

    Временно убрана фиксированная шапка

--- local/templates/exterra/js/script.js
+++ local/templates/exterra/js/script.js
@@ -1,10 +1,10 @@
-$(window).scroll(function(){
-  var sticky = $('.js-header'),
-  scroll = $(window).scrollTop();
+// $(window).scroll(function(){
+//   var sticky = $('.js-header'),
+//   scroll = $(window).scrollTop();
 
-  if (scroll >= 100) sticky.addClass('header-fixed');
-  else sticky.removeClass('header-fixed');
-});
+//   if (scroll >= 100) sticky.addClass('header-fixed');
+//   else sticky.removeClass('header-fixed');
+// });
 
 jQuery( document ).ready(function( $ ) {
   $('.custom-preview').each(function(index, element) {$clamp(element, { clamp: 6, useNativeClamp: false });});
