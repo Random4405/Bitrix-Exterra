304539d984771fc9d8a5f98215bcbe87963836a1
Random4405 <random4405@gmail.com>
2017-04-05 00:42:01 +0300

    Начал работать над страницей товаров

--- catalog/novaya-stranitsa.php
+++ catalog/novaya-stranitsa.php
@@ -3,6 +3,53 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новая страница");
 ?>
 
-Text here....
+<div class="container">
+  <div class="row">
+    <div class="half-wrapper">
+      <div class="img-wrapper"><img src="/upload/iblock/7fd/7fd085db77995c295934db50477fa503.jpg" alt=""></div>
+    </div>
+    <div class="half-wrapper">
+      <h4>Кирпич облицовочный</h4>
+      <h4>Giallo Macenato</h4>
+      <div class="half-wrapper">
+        <div class="cost">3 <sup>55</sup> за шт</div>
+        <div class="cost">48 <sup>55</sup> за за м<sup>2</sup></div>
+        <div class="sticker-new"></div>
+        <div class="sticker-in"></div>
+        <table>
+          <tr>
+            <td>Описание</td>
+            <td>Giallo Macehiato</td>
+          </tr>
+          <tr>
+            <td>Артинкул:</td>
+            <td>12409911</td>
+          </tr>
+          <tr>
+            <td>Брэнд:</td>
+            <td>Vandersanden</td>
+          </tr>
+          <tr>
+            <td>Размеры:</td>
+            <td>M50: 190x20x50мм</td>
+          </tr>
+          <tr>
+            <td>Цвет:</td>
+            <td>коричневый</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <div class="half-wrapper">
+      <div class="slick-slider">
+        <div class="slick-vertical-wrapper">1</div>
+        <div class="slick-vertical-wrapper">2</div>
+        <div class="slick-vertical-wrapper">3</div>
+        <div class="slick-vertical-wrapper">4</div>
+        <div class="slick-vertical-wrapper">5</div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
