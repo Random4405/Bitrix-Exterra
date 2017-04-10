1793407418e0b021f5bd47e2590e661a63a6ba34
Random4405 <random4405@gmail.com>
2017-04-11 00:43:53 +0300

    Изменения страницы товара

--- catalog/novaya-stranitsa.php
+++ catalog/novaya-stranitsa.php
@@ -5,51 +5,56 @@ $APPLICATION->SetTitle("Новая страница");
 
 <div class="container">
   <div class="row">
-    <div class="half-wrapper">
-      <div class="img-wrapper"><img src="/upload/iblock/7fd/7fd085db77995c295934db50477fa503.jpg" alt=""></div>
-    </div>
-    <div class="half-wrapper">
-      <h4>Кирпич облицовочный</h4>
-      <h4>Giallo Macenato</h4>
+    <div class="half-row">
       <div class="half-wrapper">
-        <div class="cost">3 <sup>55</sup> за шт</div>
-        <div class="cost">48 <sup>55</sup> за за м<sup>2</sup></div>
-        <div class="sticker-new"></div>
-        <div class="sticker-in"></div>
-        <table>
-          <tr>
-            <td>Описание</td>
-            <td>Giallo Macehiato</td>
-          </tr>
-          <tr>
-            <td>Артинкул:</td>
-            <td>12409911</td>
-          </tr>
-          <tr>
-            <td>Брэнд:</td>
-            <td>Vandersanden</td>
-          </tr>
-          <tr>
-            <td>Размеры:</td>
-            <td>M50: 190x20x50мм</td>
-          </tr>
-          <tr>
-            <td>Цвет:</td>
-            <td>коричневый</td>
-          </tr>
-        </table>
+        <div class="img-wrapper slider-for">
+          <img src="/upload/iblock/7fd/7fd085db77995c295934db50477fa503.jpg" alt="">
+        </div>
       </div>
-    </div>
-    <div class="half-wrapper">
-      <div class="slick-slider">
-        <div class="slick-vertical-wrapper">1</div>
-        <div class="slick-vertical-wrapper">2</div>
-        <div class="slick-vertical-wrapper">3</div>
-        <div class="slick-vertical-wrapper">4</div>
-        <div class="slick-vertical-wrapper">5</div>
+      <div class="half-wrapper">
+        <div class="half-row">
+          <div class="half-wrapper">
+            <h4>Кирпич облицовочный</h4>
+            <h4>Giallo Macenato</h4>
+            <div class="cost">3 <sup>55</sup> за шт</div>
+            <div class="cost">48 <sup>55</sup> за за м<sup>2</sup></div>
+            <div class="sticker-new"></div>
+            <div class="sticker-in"></div>
+            <table>
+              <tr>
+                <td>Описание</td>
+                <td>Giallo Macehiato</td>
+              </tr>
+              <tr>
+                <td>Артинкул:</td>
+                <td>12409911</td>
+              </tr>
+              <tr>
+                <td>Брэнд:</td>
+                <td>Vandersanden</td>
+              </tr>
+              <tr>
+                <td>Размеры:</td>
+                <td>M50: 190x20x50мм</td>
+              </tr>
+              <tr>
+                <td>Цвет:</td>
+                <td>коричневый</td>
+              </tr>
+            </table>
+          </div>
+          <div class="half-wrapper">
+            <div class="slider-nav">
+              <div class="slick-vertical-wrapper"><img src="/upload/iblock/7fd/7fd085db77995c295934db50477fa503.jpg" alt=""></div>
+              <div class="slick-vertical-wrapper"><img src="/upload/iblock/38b/38b46cb562d3b1966a589ab7cd5c83a3.jpg" alt=""></div>
+              <div class="slick-vertical-wrapper"><img src="/upload/iblock/691/691bdcfe0aa83250bf2afb430bcb85a3.jpg" alt=""></div>
+              <div class="slick-vertical-wrapper"><img src="/upload/iblock/72d/72dad7174e063a628aabe4c051118218.jpg" alt=""></div>
+              <div class="slick-vertical-wrapper"><img src="/upload/iblock/67d/67d377e0db067e8365f181f3ff98d9f6.jpg" alt=""></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </div>
-
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- local/templates/exterra/footer.php
+++ local/templates/exterra/footer.php
@@ -66,5 +66,24 @@ IncludeTemplateLangFile(__FILE__);
       });
     });
 </script>
+<script>
+$(document).ready(function(){
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+});
+</script>
 </body>
 </html>
