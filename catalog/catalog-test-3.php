44575b2ac91b4bd95dac4a36989ceb4247db4ced
Random4405 <random4405@gmail.com>
2017-04-14 00:43:38 +0300

    Добавлено webkit свойство для flex

---
+++ catalog/catalog-test-3.php
@@ -0,0 +1,173 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новая страница");
+?>
+
+<div class="container">
+  <div class="row">
+<?$APPLICATION->IncludeComponent(
+  "bitrix:breadcrumb",
+  "",
+  Array(
+    "PATH" => "",
+    "SITE_ID" => "s1",
+    "START_FROM" => "0"
+  )
+);?>
+  </div>
+  <div class="row flex-row">
+    <div class="content-sidebar">
+      <ul>
+        <li>Кирпич облицовочный</li>
+        <li>Клинкерный</li>
+        <li>Кирпич ручной формовки</li>
+        <li>Декоративный кирпич для
+          внутренней отделки</li>
+        <li>Распродажа</li>
+      </ul>
+    </div>
+    <div class="content-main">
+      <div class="img-wrapper">
+        <div class="texture-name">Кирпич клинкерный</div>
+        <img class="img-responsive" src="/images/catalog-qube.png" alt="">
+      </div>
+      <div class="img-wrapper">
+        <div class="texture-name">Кирпич клинкерный</div>
+        <img class="img-responsive" src="/images/catalog-qube.png" alt="">
+      </div>
+      <div class="img-wrapper">
+        <div class="texture-name">Кирпич клинкерный</div>
+        <img class="img-responsive" src="/images/catalog-qube.png" alt="">
+      </div>
+    </div>
+  </div>
+</div>
+<div class="jumbo"><img src="/images/catalog-jumbo.png" alt=""></div>
+<div class="container">
+  <div class="row">
+    <p class="desc">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend diam vel turpis efficitur suscipit. Fusce posuere dapibus neque, vel vehicula nisi iaculis consequat.
+
+    Duis feugiat risus dui, eget fermentum magna accumsan nec. Nulla laoreet commodo justo eu faucibus. Nunc tristique in ex nec mattis. Aenean malesuada ac erat auctor
+
+    dictum. Praesent lectus ipsum, sollicitudin a ultrices ut, malesuada id eros. Aenean laoreet leo metus, quis ultrices nisl cursus ac.
+
+    Vestibulum augue dolor, dignissim tincidunt magna vel, feugiat laoreet quam. Fusce eu velit rutrum, imperdiet quam a, egestas nisi. Quisque sit amet mi semper, iaculis
+
+    mauris eget, venenatis orci. Sed quis felis risus. Nullam ut posuere lectus. Nulla lacinia dui in sapien rutrum fringilla. Vivamus et lobortis dolor. Proin sit amet elit efficitur
+
+    velit vestibulum tristique.
+
+    Donec vitae mi interdum, tempor erat quis, faucibus augue. Mauris sapien lorem, rhoncus eget pharetra et, pellentesque a justo. Cras vulputate erat ipsum, et fringilla
+
+    nibh volutpat non. Donec tellus leo, suscipit sit amet varius eget, dictum in orci. Duis sapien risus, vestibulum vitae sodales quis, feugiat vel leo. Morbi pretium dolor id
+
+    felis rhoncus dapibus. Morbi at volutpat eros. Quisque risus lorem, consectetur eget lectus et, semper mollis neque. Aliquam purus lectus, tempor. Lorem ipsum dolor sit
+
+    amet, consectetur adipiscing elit. Vestibulum vel suscipit neque. Nunc interdum lobortis vestibulum. Vivamus iaculis suscipit neque eget hendrerit. Donec lacinia in ipsum
+
+    vel porttitor. Nulla ultricies cursus mauris nec gravida. Maecenas aliquam a diam eget feugiat. Nunc leo nibh, aliquet vel orci sit amet, ultrices fringilla nisl.
+
+    Donec bibendum tincidunt erat id malesuada. Nunc varius velit lorem, rhoncus lacinia ligula tristique quis. Sed tortor dui, condimentum sit amet porta molestie, dictum
+
+    condimentum lacus. Donec auctor nisl vel tortor feugiat iaculis. Fusce erat est, fringilla a condimentum id, posuere vel ligula. Integer augue velit, lobortis nec fermentum a,
+
+    sodales ut libero. Aliquam eleifend mauris a vulputate accumsan. Nunc interdum urna at purus tincidunt molestie. Nullam viverra a nisi nec elementum. Donec semper
+
+    nisi sapien, at egestas quam efficitur sed.
+
+    Maecenas turpis velit, cursus iaculis lectus mollis, malesuada gravida enim. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam blandit congue orci
+
+    quis ultricies. Nam feugiat quam et purus mollis posuere. Integer auctor est a risus consequat eleifend. Duis in erat sit amet lorem vestibulum lacinia vitae sit amet tellus.
+
+    Nullam facilisis sodales nunc consectetur fermentum. Maecenas at eros ligula. Fusce facilisis quis lorem ac interdum. Proin vel turpis ac ligula lacinia pretium. end ac.
+    </p>
+  </div>
+
+  <div class="row">
+    <h1 class="title">Популярные товары</h1>
+  </div>
+  <div class="row flex-row">
+    <div class="flex-product-wrapper">
+      <div class="img-product-wrapper"><img class="img-responsive" src="/images/sand.png" alt=""></div>
+      <h3>Название</h3>
+      <div class="product-desc">
+        <div class="left-product-wrapper">
+          <div class="product-cost">
+            <p>за шт.</p>
+            <p>17.25 P</p>
+          </div>
+          <p class="green">+ в наличии</p>
+        </div>
+        <div class="right-product-wrapper">
+          <div class="product-cost">
+            <p>за кв.м</p>
+            <p>1213.00</p>
+          </div>
+          <a class="product-button" href="#">Купить сейчас</a>
+        </div>
+      </div>
+    </div>
+    <div class="flex-product-wrapper">
+      <div class="img-product-wrapper"><img class="img-responsive" src="/images/sand.png" alt=""></div>
+      <h3>Название</h3>
+      <div class="product-desc">
+        <div class="left-product-wrapper">
+          <div class="product-cost">
+            <p>за шт.</p>
+            <p>17.25 P</p>
+          </div>
+          <p class="green">+ в наличии</p>
+        </div>
+        <div class="right-product-wrapper">
+          <div class="product-cost">
+            <p>за кв.м</p>
+            <p>1213.00</p>
+          </div>
+          <a class="product-button" href="#">Купить сейчас</a>
+        </div>
+      </div>
+    </div>
+    <div class="flex-product-wrapper">
+      <div class="img-product-wrapper"><img class="img-responsive" src="/images/sand.png" alt=""></div>
+      <h3>Название</h3>
+      <div class="product-desc">
+        <div class="left-product-wrapper">
+          <div class="product-cost">
+            <p>за шт.</p>
+            <p>17.25 P</p>
+          </div>
+          <p class="green">+ в наличии</p>
+        </div>
+        <div class="right-product-wrapper">
+          <div class="product-cost">
+            <p>за кв.м</p>
+            <p>1213.00</p>
+          </div>
+          <a class="product-button" href="#">Купить сейчас</a>
+        </div>
+      </div>
+    </div>
+    <div class="flex-product-wrapper">
+      <div class="img-product-wrapper"><img class="img-responsive" src="/images/sand.png" alt=""></div>
+      <h3>Название</h3>
+      <div class="product-desc">
+        <div class="left-product-wrapper">
+          <div class="product-cost">
+            <p>за шт.</p>
+            <p>17.25 P</p>
+          </div>
+          <p class="green">+ в наличии</p>
+        </div>
+        <div class="right-product-wrapper">
+          <div class="product-cost">
+            <p>за кв.м</p>
+            <p>1213.00</p>
+          </div>
+          <a class="product-button" href="#">Купить сейчас</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
