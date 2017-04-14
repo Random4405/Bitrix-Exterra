25b7d635575a7aaa224832589af913c1ef750acf
Random4405 <random4405@gmail.com>
2017-04-14 22:44:53 +0300

    Доделана 4-ая страница каталога

---
+++ catalog/catalog-test-4.php
@@ -0,0 +1,323 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новая страница");
+?>
+
+<div class="container">
+  <div class="row">
+    <?$APPLICATION->IncludeComponent(
+      "bitrix:breadcrumb",
+      "",
+      Array(
+        "PATH" => "",
+        "SITE_ID" => "s1",
+        "START_FROM" => "0"
+      )
+    );?>
+  </div>
+<div class="row flex-row mb16">
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+</div>
+<div class="row flex-row mb16">
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+</div>
+<div class="row flex-row mb16">
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+  <div class="catalog-category-wrapper">
+    <div class="img-wrapper">
+      <img src="/images/test-image.png" alt="">
+      <span class="texture-name">Кирпич</span>
+    </div>
+  </div>
+</div>
+</div>
+<div class="subcategory-main">
+<div class="subcategory-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="left-wrapper">
+        <h3>Название товара</h3>
+        <ul>
+          <li>Пункт списка</li>
+          <li>Пункт списка</li>
+          <li>Пункт списка</li>
+          <li>Пункт списка</li>
+        </ul>
+      </div>
+      <div class="right-wrapper">
+        <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+      </div>
+    </div>
+  </div>
+</div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>
+              <span>Название подсписка</span>
+              <ul>
+                <li>Подсписок</li>
+                <li>Подсписок</li>
+                <li>Подсписок</li>
+                <li>Подсписок</li>
+              </ul>
+            </li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="subcategory-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="left-wrapper">
+          <h3>Название товара</h3>
+          <ul>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+            <li>Пункт списка</li>
+          </ul>
+        </div>
+        <div class="right-wrapper">
+          <div class="img-wrapper"><img src="/images/test-image-large.png" alt=""></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
