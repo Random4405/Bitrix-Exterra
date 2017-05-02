114f1200b8d11cdfade4dd2ad97aa6bc6c513d43
Random4405 <random4405@gmail.com>
2017-05-02 13:51:48 +0300

    Исправлен выбор элементов для выпадающего меню

--- local/templates/exterra/js/script.js
+++ local/templates/exterra/js/script.js
@@ -37,7 +37,7 @@ function openCity(evt, cityName) {
 
 jQuery( document ).ready(function( $ ) {
   $('button.tablinks').first().addClass('active');
-  $("#horizontal-multilevel-menu > li:first-child > a ").click(function(){
+  $("header #horizontal-multilevel-menu > li:first-child > a ").click(function(){
     $("#horizontal-multilevel-menu").toggleClass("open");
     return false;
   });
