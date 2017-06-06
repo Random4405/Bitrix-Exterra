c6da2c244e5c77ce9f64b35b7b0b5108b5452bcf
Random4405 <random4405@gmail.com>
2017-06-06 20:35:17 +0300

    Изменены классы товаров

--- local/templates/exterra/components/bitrix/catalog.section/featured/template.php
+++ local/templates/exterra/components/bitrix/catalog.section/featured/template.php
@@ -16,57 +16,57 @@ $this->setFrameMode(true);
 <?
 if (!empty($arResult['ITEMS']))
 {
-$num = 0;
-foreach ($arResult['ITEMS'] as $key => $arItem)
-{
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], $strElementEdit);
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], $strElementDelete, $arElementDeleteParams);
-	$strMainID = $this->GetEditAreaId($arItem['ID']);
+  $num = 0;
+  foreach ($arResult['ITEMS'] as $key => $arItem)
+  {
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], $strElementEdit);
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], $strElementDelete, $arElementDeleteParams);
+    $strMainID = $this->GetEditAreaId($arItem['ID']);
 
-	$productTitle = (
-		isset($arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'])&& $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'] != ''
-		? $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE']
-		: $arItem['NAME']
-	);
+    $productTitle = (
+      isset($arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'])&& $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'] != ''
+      ? $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE']
+      : $arItem['NAME']
+    );
 
-	$file = CFile::ResizeImageGet($arItem["PREVIEW_PICTURE"]['ID'], array('width'=>288, 'height'=>288), BX_RESIZE_IMAGE_EXACT , true);
+    $file = CFile::ResizeImageGet($arItem["PREVIEW_PICTURE"]['ID'], array('width'=>288, 'height'=>288), BX_RESIZE_IMAGE_EXACT , true);
 
-	if ($num == 0) echo '<div class="row product-arrows">';
-	?>
+    if ($num == 0) echo '<div class="row product-arrows">';
+    ?>
 
-      <div class="flex-product-wrapper" id="<? echo $strMainID; ?>">
-			<?if ($arItem['DISPLAY_PROPERTIES']['nalichie']['VALUE']):?>
-						<p class="green">в наличии</p>
-			<?endif;?>
-        <div class="img-product-wrapper"><a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>"><img class="img-responsive" src="<?=$file['src']?>" alt=""></a></div>
-        <h3><a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>"><? echo $productTitle; ?></a></h3>
-        <div class="product-desc">
-          <div class="left-product-wrapper">
-			<?if ($arItem['DISPLAY_PROPERTIES']['price_sht']['VALUE']):?>
-            <div class="product-cost">
-              <p><?=$arItem['DISPLAY_PROPERTIES']['price_sht']['VALUE']?></p>
-            </div>
-			<?endif;?>
-      <?if ($arItem['DISPLAY_PROPERTIES']['price_kvm']['VALUE']):?>
-            <div class="product-cost">
-              <p><?=$arItem['DISPLAY_PROPERTIES']['price_kvm']['VALUE']?></p>
-            </div>
+    <div class="product-card" id="<? echo $strMainID; ?>">
+      <?if ($arItem['DISPLAY_PROPERTIES']['nalichie']['VALUE']):?>
+      <p class="product-card--green-badge">в наличии</p>
       <?endif;?>
+      <div class="product-card--image"><a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>"><img class="img-responsive" src="<?=$file['src']?>" alt=""></a></div>
+      <h3><a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>"><? echo $productTitle; ?></a></h3>
+      <div class="product-card--props">
+        <div class="l-left props--cost">
+          <?if ($arItem['DISPLAY_PROPERTIES']['price_sht']['VALUE']):?>
+          <div class="cost--wrapper">
+            <p><?=$arItem['DISPLAY_PROPERTIES']['price_sht']['VALUE']?></p>
           </div>
-          <div class="right-product-wrapper">
-            <a class="product-button" href="#" onclick="event.preventDefault();buyForm('<?=$arItem['NAME']?>');">Купить сейчас</a>
+          <?endif;?>
+          <?if ($arItem['DISPLAY_PROPERTIES']['price_kvm']['VALUE']):?>
+          <div class="cost--wrapper">
+            <p><?=$arItem['DISPLAY_PROPERTIES']['price_kvm']['VALUE']?></p>
           </div>
+          <?endif;?>
+        </div>
+        <div class="l-right props--button">
+          <a class="product-button" href="#" onclick="event.preventDefault();buyForm('<?=$arItem['NAME']?>');">Купить сейчас</a>
         </div>
       </div>
+    </div>
 
-<?
-$num++;
-if ($num == 7) {
-	$num = 0;
-	echo '</div>';
-}
-}
-if ($num > 0) echo '</div>';
+    <?
+    $num++;
+    if ($num == 7) {
+      $num = 0;
+      echo '</div>';
+    }
+  }
+  if ($num > 0) echo '</div>';
 
 }
 ?>
