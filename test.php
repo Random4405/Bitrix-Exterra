7ea83266d3915342f03224fe40f1a51063caf5a2
Random4405 <random4405@gmail.com>
2017-06-12 00:50:30 +0300

    Добавлена тестовая страница заголовков

---
+++ test.php
@@ -0,0 +1,13 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "Экстерра Групп | Продажа строительных материалов премиум класса");
+$APPLICATION->SetTitle("Экстерра Групп | Продажа строительных материалов премиум класса");
+?>
+
+<h1 style="text-align: center">h1: Hello World!</h1>
+<h2 style="text-align: center">h2: Hello World!</h2>
+<h3 style="text-align: center">h3: Hello World!</h3>
+<h4 style="text-align: center">h4: Hello World!</h4>
+<h5 style="text-align: center; display: block">h5: Hello World!</h5>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
