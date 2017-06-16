233792d224657888f48506c18f83aa8167281a84
Random4405 <random4405@gmail.com>
2017-06-16 22:48:05 +0300

    Server changes

--- baza-znaniy/index.php
+++ baza-znaniy/index.php
@@ -12,6 +12,14 @@ $APPLICATION->SetTitle("Title");
 <div style="float: left; display: block; width: 50%;">
 <iframe width="560" height="315" src="https://www.youtube.com/embed/kmvFmteu70c" frameborder="0" allowfullscreen></iframe>
 </div>
+				<br />
+<div style="float: left; display: block; width: 50%;">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/8Kyc9bNpSA8" frameborder="0" allowfullscreen></iframe>
+</div>
+<div style="float: left; display: block; width: 50%;">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/TGDr675SbQ0" frameborder="0" allowfullscreen></iframe>
+</div>
+
 </div>
 </div>
 </div>
