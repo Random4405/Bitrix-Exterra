1135177f0422e4c53d6549cb30dc23dc24329765
Random4405 <random4405@gmail.com>
2017-06-17 02:43:54 +0300

    Добавлены отступы на страницу База Знаний

--- baza-znaniy/index.php
+++ baza-znaniy/index.php
@@ -3,8 +3,8 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Title");
 ?>
 <div class="container">
-<div class="row">
- <h1 class="is-no-top-margin">База знаний</h1>
+<div class="row gutter-bottom">
+ <h1>База знаний</h1>
             <div class="paragraph">
 <div style="float: left; display: block; width: 50%;">
 <iframe width="560" height="315" src="https://www.youtube.com/embed/rbKYtfB3GsU" frameborder="0" allowfullscreen></iframe>
