9df43ec78acd07d4e0e923da849bc253b8a86b39
Random4405 <random4405@gmail.com>
2017-06-16 22:44:52 +0300

    Server changes

--- baza-znaniy/index.php
+++ baza-znaniy/index.php
@@ -2,5 +2,17 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Title");
 ?>
+<div class="container">
+<div class="row">
+ <h1 class="is-no-top-margin">База знаний</h1>
+            <div class="paragraph">
+<div style="float: left; display: block; width: 50%;">
 <iframe width="560" height="315" src="https://www.youtube.com/embed/rbKYtfB3GsU" frameborder="0" allowfullscreen></iframe>
+</div>
+<div style="float: left; display: block; width: 50%;">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/kmvFmteu70c" frameborder="0" allowfullscreen></iframe>
+</div>
+</div>
+</div>
+</div>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
