2e870c2961a3ec5f1f0c7ed003479a0cf997a040
Random4405 <random4405@gmail.com>
2017-06-20 23:46:19 +0300

    Добавлена страница галереи

---
+++ catalog/gallery1.php
@@ -0,0 +1,75 @@
+
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новая страница");
+?>
+
+<div class="container">
+  <div class="row">
+<?$APPLICATION->IncludeComponent(
+  "bitrix:breadcrumb",
+  "",
+  Array(
+    "PATH" => "",
+    "SITE_ID" => "s1",
+    "START_FROM" => "0"
+  )
+);?>
+</div>
+  <div class="row flex-row flex-row__wrap">
+<div class="category-wrapper mb10">
+  <div class="img-wrapper">
+    <a href=""><span class="texture-name">Натуральный сланец</span>
+      <img src="/upload/iblock/eaa/eaa94d99b7f047c8b6b84258dce974e0.jpg" alt="">
+    </a>
+  </div>
+</div>
+<div class="category-wrapper mb10">
+  <div class="img-wrapper">
+    <a href=""><span class="texture-name">Натуральный сланец</span>
+      <img src="/upload/iblock/eaa/eaa94d99b7f047c8b6b84258dce974e0.jpg" alt="">
+    </a>
+  </div>
+</div>
+<div class="category-wrapper mb10">
+  <div class="img-wrapper">
+    <a href=""><span class="texture-name">Натуральный сланец</span>
+      <img src="/upload/iblock/eaa/eaa94d99b7f047c8b6b84258dce974e0.jpg" alt="">
+    </a>
+  </div>
+</div>
+<div class="category-wrapper mb10">
+  <div class="img-wrapper">
+    <a href=""><span class="texture-name">Натуральный сланец</span>
+      <img src="/upload/iblock/eaa/eaa94d99b7f047c8b6b84258dce974e0.jpg" alt=""></a>
+    </div>
+</div>
+<div class="category-wrapper mb10">
+  <div class="img-wrapper">
+    <a href=""><span class="texture-name">Натуральный сланец</span>
+      <img src="/upload/iblock/eaa/eaa94d99b7f047c8b6b84258dce974e0.jpg" alt=""></a>
+    </div>
+</div>
+<div class="category-wrapper mb10">
+  <div class="img-wrapper">
+    <a href=""><span class="texture-name">Натуральный сланец</span>
+      <img src="/upload/iblock/eaa/eaa94d99b7f047c8b6b84258dce974e0.jpg" alt=""></a>
+    </div>
+</div>
+<div class="category-wrapper mb10">
+  <div class="img-wrapper">
+    <a href=""><span class="texture-name">Натуральный сланец</span>
+      <img src="/upload/iblock/eaa/eaa94d99b7f047c8b6b84258dce974e0.jpg" alt=""></a>
+    </div>
+</div>
+<div class="category-wrapper mb10">
+  <div class="img-wrapper">
+    <a href=""><span class="texture-name">Натуральный сланец</span>
+      <img src="/upload/iblock/eaa/eaa94d99b7f047c8b6b84258dce974e0.jpg" alt=""></a>
+    </div>
+</div>
+</div>
+</div>
+
+<div class="mb70"></div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
