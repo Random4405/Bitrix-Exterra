997dfc91c7cc84e02fccb65ba1a5a7b62609ef8c
Random4405 <random4405@gmail.com>
2017-07-17 23:48:28 +0300

    Статьи: дата и заголовок поменяли местами

--- local/templates/exterra/components/bitrix/news/list_of_articles/bitrix/news.list/.default/template.php
+++ local/templates/exterra/components/bitrix/news/list_of_articles/bitrix/news.list/.default/template.php
@@ -45,9 +45,6 @@ $this->setFrameMode(true);
 					/>
 			<?endif;?>
 		<?endif?>
-		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
-		<?endif?>
 		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
 			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
 				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><h2><?echo $arItem["NAME"]?></h2></a><br />
@@ -55,6 +52,9 @@ $this->setFrameMode(true);
 				<h2><?echo $arItem["NAME"]?></h2><br />
 			<?endif;?>
 		<?endif;?>
+    <?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
+      <span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
+    <?endif?>
 		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
 			<div class="news-item__prewiew-text"><?echo $arItem["PREVIEW_TEXT"];?></div>
 		<?endif;?>
