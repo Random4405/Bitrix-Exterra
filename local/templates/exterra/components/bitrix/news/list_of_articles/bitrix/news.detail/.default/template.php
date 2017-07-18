0acbf0b3a9b54bca3d82b6ca57cad43139083905
Random4405 <random4405@gmail.com>
2017-07-18 23:08:00 +0300

    Удалена картинка и дата из детального шаблона новости

--- local/templates/exterra/components/bitrix/news/list_of_articles/bitrix/news.detail/.default/template.php
+++ local/templates/exterra/components/bitrix/news/list_of_articles/bitrix/news.detail/.default/template.php
@@ -13,20 +13,6 @@
 $this->setFrameMode(true);
 ?>
 <div class="news-detail">
-	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
-		<img
-			class="detail_picture"
-			border="0"
-			src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
-			width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
-			height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
-			alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
-			title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
-			/>
-	<?endif?>
-	<?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
-		<span class="news-date-time"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></span>
-	<?endif;?>
 	<?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
 		<h3><?=$arResult["NAME"]?></h3>
 	<?endif;?>
