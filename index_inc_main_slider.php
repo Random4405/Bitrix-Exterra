b2e716ae55f1411f3e9ba70c0339178fe9bc187e
Random4405 <random4405@gmail.com>
2017-07-27 00:23:58 +0300

    Slider changes

--- index_inc_main_slider.php
+++ index_inc_main_slider.php
@@ -10,12 +10,12 @@
     </style>
   <div class="hero--slick" data-slick="{&quot;infinite&quot;: true, &quot;arrows&quot;: false, &quot;autoplay&quot;: true, &quot;autoplaySpeed&quot;: 4000, &quot;dots&quot;: true}">
     <div class="hero-slide">
+    <div class="absolute-wrapper--button"><a href="http://exterragroup.ru/o-kompanii/">Узнать больше</a></div>
   <div class="hero--absolute-wrapper">
     <div class="absolute-wrapper--caption">
       <img src="/images/text-logo.png" alt="">
       <p>только качественные<br>строительные материалы</p>
     </div>
-    <div class="absolute-wrapper--button"><a href="http://exterragroup.ru/o-kompanii/">Узнать больше</a></div>
   </div>
   <img src="/images/slider1.jpg" style="filter: brightness(0.5);" class="img-responsive" alt="">
 </div>
