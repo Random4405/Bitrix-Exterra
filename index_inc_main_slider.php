59ca4c15493dad1716f6ec7031609a74edee2875
Random4405 <random4405@gmail.com>
2017-07-26 23:35:53 +0300

    Test commit

--- index_inc_main_slider.php
+++ index_inc_main_slider.php
@@ -1,4 +1,8 @@
 <div class="hero">
+
+  <div class="hero--slick" data-slick="{&quot;infinite&quot;: true, &quot;arrows&quot;: false, &quot;autoplay&quot;: true, &quot;autoplaySpeed&quot;: 4000, &quot;dots&quot;: true}">
+    <div class="hero-slide">
+
   <div class="hero--absolute-wrapper">
     <div class="absolute-wrapper--caption">
       <img src="/images/text-logo.png" alt="">
@@ -6,9 +10,7 @@
     </div>
     <div class="absolute-wrapper--button"><a href="http://exterragroup.ru/o-kompanii/">Узнать больше</a></div>
   </div>
-
-  <div class="hero--slick" data-slick="{&quot;infinite&quot;: true, &quot;arrows&quot;: false, &quot;autoplay&quot;: true, &quot;autoplaySpeed&quot;: 4000, &quot;dots&quot;: true}">
-    <div class="hero-slide"><img src="/images/slider1.jpg" class="img-responsive" alt=""></div>
+<img src="/images/slider1.jpg" class="img-responsive" alt=""></div>
     <div class="hero-slide"><img src="/images/slider1-6.jpg" class="img-responsive" alt=""></div>
     <div class="hero-slide"><img src="/images/slider1-7.jpg" class="img-responsive" alt=""></div>
     <div class="hero-slide"><img src="/images/slider1-5.jpg" class="img-responsive" alt=""></div>
