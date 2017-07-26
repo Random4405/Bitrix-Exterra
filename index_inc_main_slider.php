a92a973008d4602ef415433974effe0e4e8de7f2
Random4405 <random4405@gmail.com>
2017-07-27 00:06:37 +0300

    Some dirty code

--- index_inc_main_slider.php
+++ index_inc_main_slider.php
@@ -2,7 +2,14 @@
 
   <div class="hero--slick" data-slick="{&quot;infinite&quot;: true, &quot;arrows&quot;: false, &quot;autoplay&quot;: true, &quot;autoplaySpeed&quot;: 4000, &quot;dots&quot;: true}">
     <div class="hero-slide">
-
+    <style>
+    .hero--slick {
+        background-color: transparent !important;
+    }
+    .hero--slick > * {
+    opacity: 1 !important;
+    }
+    </style>
   <div class="hero--absolute-wrapper">
     <div class="absolute-wrapper--caption">
       <img src="/images/text-logo.png" alt="">
@@ -10,12 +17,44 @@
     </div>
     <div class="absolute-wrapper--button"><a href="http://exterragroup.ru/o-kompanii/">Узнать больше</a></div>
   </div>
-<div class='is-shaded' style="background-color: rgba(0,0,0,.5);">
-  <img src="/images/slider1.jpg" class="img-responsive" alt="">
+  <img src="/images/slider1.jpg" style="filter: brightness(0.5);" class="img-responsive" alt="">
 </div>
+    <div class="hero-slide">
+  <div class="hero--absolute-wrapper">
+    <div class="absolute-wrapper--caption">
+      <img src="/images/text-logo.png" alt="">
+      <p>только качественные<br>строительные материалы</p>
+    </div>
+    <div class="absolute-wrapper--button"><a href="http://exterragroup.ru/o-kompanii/">Узнать больше</a></div>
+  </div>
+  <div class="hero--absolute-wrapper">
+    <div class="absolute-wrapper--caption">
+      <img src="/images/text-logo.png" alt="">
+      <p>только качественные<br>строительные материалы</p>
+    </div>
+    <div class="absolute-wrapper--button"><a href="http://exterragroup.ru/o-kompanii/">Узнать больше</a></div>
+  </div>
+<img src="/images/slider1-6.jpg" style="filter: brightness(0.5);" class="img-responsive" alt="">
 </div>
-    <div class="hero-slide"><img src="/images/slider1-6.jpg" class="img-responsive" alt=""></div>
-    <div class="hero-slide"><img src="/images/slider1-7.jpg" class="img-responsive" alt=""></div>
-    <div class="hero-slide"><img src="/images/slider1-5.jpg" class="img-responsive" alt=""></div>
+    <div class="hero-slide">
+  <div class="hero--absolute-wrapper">
+    <div class="absolute-wrapper--caption">
+      <img src="/images/text-logo.png" alt="">
+      <p>только качественные<br>строительные материалы</p>
+    </div>
+    <div class="absolute-wrapper--button"><a href="http://exterragroup.ru/o-kompanii/">Узнать больше</a></div>
+  </div>
+<img src="/images/slider1-7.jpg"  style="filter: brightness(0.5);" class="img-responsive" alt="">
+</div>
+    <div class="hero-slide">
+  <div class="hero--absolute-wrapper">
+    <div class="absolute-wrapper--caption">
+      <img src="/images/text-logo.png" alt="">
+      <p>только качественные<br>строительные материалы</p>
+    </div>
+    <div class="absolute-wrapper--button"><a href="http://exterragroup.ru/o-kompanii/">Узнать больше</a></div>
+  </div>
+<img src="/images/slider1-5.jpg"  style="filter: brightness(0.5);" class="img-responsive" alt="">
+</div>
   </div>
 </div><br>
